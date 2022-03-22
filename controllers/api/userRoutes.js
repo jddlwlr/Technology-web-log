5261d7d1eae59bbefe500f7c35592ad966f4b23e
Jesse Lawler <jedd.lawler@gmail.com>
2022-03-22 14:58:31 -0600

    trying to get heroku running

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("/models/user");
+const User = require("../../models/user");
 
 //create a new user
 router.post("/", async (req, res) => {
